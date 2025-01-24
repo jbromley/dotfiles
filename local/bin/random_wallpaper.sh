49d00f2ea2f3b642ce938166b83baabc9936ee79
J. Bromley <jay@appliedinvention.com>
2025-01-24 14:24:57 -0800

    Don't use thumbnails for wallpapers
    Some tools create a .thumbnails directory and the random_wallpaper.sh
    script was sometimes selecting files from that directory for the
    wallpaper, resulting in a very pixelated background. Limited image
    search depth to 1 directory to not pick up files in the .thumbnails
    directory.

--- local/bin/random_wallpaper.sh
+++ local/bin/random_wallpaper.sh
@@ -3,19 +3,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly WALLPAPER_DIR="${HOME}/Pictures/Wallpaper"
+readonly wallpaper_dir="${HOME}/Pictures/Wallpaper"
 
 get_x_display() {
-    ps -u $(id -u) -o pid= | \
-	while read pid; do
-	    cat /proc/$pid/environ 2>/dev/null | tr '\0' '\n' | grep '^DISPLAY=:'
-	done | grep -o ':[0-9]*' | sort -u
+  ps -u $(id -u) -o pid= |
+    while read pid; do
+      cat /proc/$pid/environ 2>/dev/null | tr '\0' '\n' | grep '^DISPLAY=:'
+    done | grep -o ':[0-9]*' | sort -u
 }
 
 export DISPLAY=$(get_x_display)
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
 
-wallpaper=$(find "${WALLPAPER_DIR}" -type f | shuf -n 1)
+wallpaper=$(find "${wallpaper_dir}" -maxdepth 1 -type f | shuf -n 1)
 
 feh --bg-fill "${wallpaper}"
 ${HOME}/.local/bin/make_lockscreen.sh ${wallpaper} &
