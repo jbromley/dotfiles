30cc8a737a445e678402dbea1c5b5274bc533974
J. Bromley <jbromley@gmail.com>
2025-02-06 14:01:08 -0800

    Turn off autoformat in Neovim
    The default for LazyVim was to automatically format source code when
    saving it. Turned this off to avoid inadvertently reformatted code and
    causing commits with hundreds of whitespace changes.

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,8 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.autoformat = false
+
+-- Set options for GUI clients
+vim.opt.guifont = "JetBrains Mono:h10"
 vim.g.neovide_transparency = 0.96875
 vim.g.neovide_window_blurred = true
-
--- Set the font for GUI clients.
-vim.opt.guifont = "JetBrains Mono:h10"
