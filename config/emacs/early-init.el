0611672c1a745ae3e2d3fc2e36c9608efaaa9abb
J. Bromley <jay@appliedinvention.com>
2025-02-25 15:44:57 -0800

    Use setopt in early-init.el
    Ensure that customization code is executed.

--- config/emacs/early-init.el
+++ config/emacs/early-init.el
@@ -4,36 +4,40 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(setq use-package-compute-statistics t)
+
 ;; Configure GC for fast initialization.
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.5)
 
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (setq gc-cons-threshold (* 8 1024 1024)
-                  gc-cons-percentage 0.1)))
+(defun jb/emacs-startup ()
+  (setq gc-cons-threshold (* 16 1024 1024)
+        gc-cons-percentage 0.1))
+
+(add-hook 'emacs-startup-hook #'jb/emacs-startup)
 
 ;; Basic frame settings
-(setq frame-resize-pixelwise t
-      frame-inhibit-implied-resize t
-      frame-title-format '("%b")
+(setopt frame-resize-pixelwise t
+        frame-inhibit-implied-resize t
+        use-dialog-box t
+        use-file-dialog nil
+        use-short-answers t
+        inhibit-splash-screen t
+        inhibit-startup-screen t
+        inhibit-startup-echo-area-message user-login-name
+        inhibit-startup-buffer-menu t)
+
+(setq frame-title-format '("%b")
       ring-bell-function 'ignore
-      use-dialog-box t ; only for mouse events, which I seldom use
-      use-file-dialog nil
-      use-short-answers t
-      inhibit-splash-screen t
-      inhibit-startup-screen t
-      inhibit-x-resources t
-      inhibit-startup-echo-area-message user-login-name ; read the docstring
-      inhibit-startup-buffer-menu t)
+      inhibit-x-resources t)
 
 ;; Startup speed, annoyance suppression
-(setq byte-compile-warnings '(not obsolete))
-(setq warning-suppress-log-types '((comp) (bytecomp)))
-(setq native-comp-async-report-warnings-errors 'silent)
+(setopt byte-compile-warnings (not 'obsolete)
+        warning-suppress-log-types '((comp) (bytecomp))
+        native-comp-async-report-warnings-errors 'silent)
 
 ;; Silence startup message
-(setq inhibit-startup-echo-area-message (user-login-name))
+(setopt inhibit-startup-echo-area-message (user-login-name))
 
 ;; Default frame configuration: full screen, good-looking title bar on macOS
 (menu-bar-mode -1)
