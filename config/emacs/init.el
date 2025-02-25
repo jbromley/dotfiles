bfd395c80dba345da1c9d2be2563be13adad51f4
J. Bromley <jay@appliedinvention.com>
2025-02-25 15:46:10 -0800

    Move Emacs configuration to top-level code
    Move base Emacs configuration out of a `(use-package emacs ...)` stanza
    and into the top level of code.

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -10,95 +10,64 @@
 (unless (server-running-p)
   (server-start))
 
-;;; Basic Emasc settings
+;;; Basic Emacs settings
 
-(use-package emacs
-  :config
-  (global-auto-revert-mode)
-  (savehist-mode)
-  (windmove-default-keybindings 'shift) ; You can use other modifiers here
-
-  ;; Make right-click do something sensible
-  (when (display-graphic-p)
-    (context-menu-mode))
-
-  (blink-cursor-mode 20)
-  (pixel-scroll-precision-mode)
-
-  ;; Global key binds
-  (global-set-key (kbd "C-z") #'zap-up-to-char)
-  ;; TAB acts more like how it does in the shell
-  (keymap-set minibuffer-mode-map "TAB" 'minibuffer-complete)
-
-  ;; Add our configuration subdirectory to load-path.
-  (add-to-list 'load-path (expand-file-name "extras" user-emacs-directory))
-
-  :custom
-  (set-mark-command-repeat-pop t)
-  (initial-major-mode 'fundamental-mode)
-  (display-time-default-load-average nil)
-  (auto-revert-avoid-polling t)
-  (auto-revert-interval 5)
-  (auto-revert-check-vc-info t)
-  (sentence-end-double-space nil)
-
-  ;; Minibuffer/completion settings
-  ;; For help, see: https://www.masteringemacs.org/article/understanding-minibuffer-completion
-  (enable-recursive-minibuffers t)
-  (completion-cycle-threshold 1)
-  (completions-detailed t)             ; Show annotations with completions.
-  (tab-always-indent 'complete)
-  (completion-styles '(basic initials substring))
-  (completion-auto-help 'always)
-  (completions-max-height 20)
-  (completions-detailed t)
-  (completions-format 'one-column)
-  (completions-group t)
-  (completion-auto-select 'second-tab)
+(setopt set-mark-command-repeat-pop t)
+(setopt initial-major-mode 'fundamental-mode)
+(setopt display-time-default-load-average nil)
+(setopt auto-revert-avoid-polling t
+        auto-revert-interval 5
+        auto-revert-check-vc-info t)
+(setopt sentence-end-double-space nil)
+;; Minibuffer/completion settings
+;; For help, see: https://www.masteringemacs.org/article/understanding-minibuffer-completion
+(setopt enable-recursive-minibuffers t
+        completion-cycle-threshold 1
+        completions-detailed t             ; Show annotations with completions.
+        tab-always-indent 'complete
+        completion-styles '(basic initials substring)
+        completion-auto-help 'always
+        completions-max-height 20
+        completions-detailed t
+        completions-format 'one-column
+        completions-group t
+        completion-auto-select 'second-tab)
   ;; (completion-auto-select t))
+(setopt line-number-mode t
+        column-number-mode t)
+(setopt x-underline-at-descent-line nil)
+(setopt switch-to-buffer-obey-display-actions t)
+(setopt show-trailing-whitespace nil)
+(setopt indicate-buffer-boundaries 'left)
+(setopt mouse-wheel-tilt-scroll t
+        mouse-wheel-flip-direction t)
+(setopt indent-tabs-mode nil
+        tab-width 4)
+(setopt tab-bar-show 1)
+(setopt display-line-numbers-width 3)
+(setopt custom-file "~/.config/emacs/custom.el")
 
-  ;; Show line and column in mode line.
-  (line-number-mode t)
-  (column-number-mode t)
+;; Emacs configuration functions
+(global-auto-revert-mode)
+(savehist-mode)
+(windmove-default-keybindings 'shift) ; You can use other modifiers here
+(when (display-graphic-p)
+  (context-menu-mode))
+(blink-cursor-mode 20)
+(pixel-scroll-precision-mode)
 
-  ;; Prettier underlines
-  (x-underline-at-descent-line nil)
-  ;; Make switching buffers more consistent
-  (switch-to-buffer-obey-display-actions t)
+;; Global key binds
+(global-set-key (kbd "C-z") #'zap-up-to-char)
+(keymap-set minibuffer-mode-map "TAB" 'minibuffer-complete)
 
-  ;; By default, don't underline trailing spaces
-  (show-trailing-whitespace nil)
+;; Add our configuration subdirectory to load-path.
+(add-to-list 'load-path (expand-file-name "extras" user-emacs-directory))
 
-  ;; Show buffer top and bottom in the margin
-  (indicate-buffer-boundaries 'left)
-
-  ;; Enable horizontal scrolling
-  (mouse-wheel-tilt-scroll t)
-  (mouse-wheel-flip-direction t)
-
-  ;; Tab settings
-  (indent-tabs-mode nil)
-  (tab-width 4)
-
-  ;; Only show tab-bar if there is more than one tab.
-  (tab-bar-show 1)
-  
-  ;; Width of line numbers in fringe.
-  (display-line-numbers-width 3)
-
-  ;; Save customizations in their own file.
-  (custom-file "~/.config/emacs/custom.el")
-
-  :hook
-  ;; Display line numbers in programming modes.
-  (prog-mode . display-line-numbers-mode)
-
-  ;; Use automatic word wrap in text modes.
-  (text-mode . visual-line-mode)
-
-  ;; Highlight current line in any text mode.
-  ;; (prog-mode . hl-line-mode)
-  (text-mode . hl-line-mode))
+;; Emacs hooks
+;; Display line numbers in programming modes.
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+(add-hook 'text-mode-hook #'visual-line-mode)
+(add-hook 'text-mode-hook #'hl-line-mode)
 
 ;;; Welcome screen
 
