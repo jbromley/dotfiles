330f4109ffcf10f04902afd5863c4b6d1b868957
J. Bromley <jay@appliedinvention.com>
2025-08-21 16:10:08 -0700

    Fixed typo in zp.fish
    Left out $ in $args.

--- config/fish/functions/zp.fish
+++ config/fish/functions/zp.fish
@@ -1,6 +1,6 @@
 function zp --description 'push a directory while using zoxide'
     set args (count $argv)
-    if test args -eq 1
+    if test $args -eq 1
         set dir (zoxide query $argv[1])
         if test $status -eq 0
             pushd $dir
