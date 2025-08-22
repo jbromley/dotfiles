71bbd1efbc6df296fe317f3bf83e02b6c8e93906
J. Bromley <jay@appliedinvention.com>
2025-08-22 00:10:11 -0700

    Moved fish abbreviations into their own file
    Moved to conf.d/abbr.fish.

---
+++ config/fish/conf.d/abbr.fish
@@ -0,0 +1,16 @@
+# Abbreviations
+abbr --add b bat
+abbr --add bc bc -ql
+abbr --add gadd git add
+abbr --add gco git checkout
+abbr --add gcom git commit
+abbr --add gp git pull
+abbr --add gP git push
+abbr --add lzg lazygit
+abbr --add vimg kitten icat
+abbr --add kfont kitten choose-fonts
+abbr --add ktheme kitten themes
+abbr --add dboxenter distrobox enter
+abbr --add dboxstop distrobox stop
+abbr --add ardcli arduino-cli
+abbr --add psg pgrep -a
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,28 +1,8 @@
 if status is-interactive
-    # Abbreviations
-    abbr --add b bat
-    abbr --add bc bc -ql
-
-    abbr --add gadd git add
-    abbr --add gco git checkout
-    abbr --add gcom git commit
-    abbr --add gpull git pull
-
-    abbr --add vimg kitten icat
-    abbr --add kfont kitten choose-fonts
-    abbr --add ktheme kitten themes
-
-    abbr --add dboxenter distrobox enter
-    abbr --add dboxstop distrobox stop
-
-    abbr --add ardcli arduino-cli
-    abbr --add psg pgrep -a
-
     # Commands to run in interactive sessions can go here
     fzf --fish | source
     eval "$(zoxide init fish)"
     mise activate fish | source
     source $HOME/.atuin/bin/env.fish
     atuin init fish | source
-    eval "$(starship init fish)"
 end
