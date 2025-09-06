e1dd673e4fa62bf8bf0cdd7135fa8fc8a02b0b01
J. Bromley <jbromley@gmail.com>
2025-09-05 19:11:14 -0700

    Added fish completions for dprint

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     fish_add_path --path $HOME/.local/bin
 
     # Commands to run in interactive sessions can go here
+    dprint completions fish | source
     fzf --fish | source
     eval "$(zoxide init fish)"
     mise activate fish | source
