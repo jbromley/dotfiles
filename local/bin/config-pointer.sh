e60739aa0053651fe59a85a43d2b1d376efb895a
J. Bromley <jbromley@gmail.com>
2025-09-15 10:36:12 -0700

    Added script to configure pointers
    Make any attached pointer devices use natural scrolling.

---
+++ local/bin/config-pointer.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+for ptr in 'Logitech 510' 'Logitech USB Receiver Mouse'; do
+    if xinput | grep -q "${ptr}"; then
+        xinput set-prop "${ptr}" "libinput Natural Scrolling Enabled" 1
+    fi
+done
