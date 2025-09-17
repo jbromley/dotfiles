079d8612c9530501b8d581efa7797925d3e74361
J. Bromley <jbromley@gmail.com>
2025-09-16 20:42:49 -0700

    Removed polybar configuration files
    Polybar does not cooperate well with i3.

--- local/bin/polybar-launch.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-# Terminate currently running polybars
-polybar-msg cmd quit
-
-# Launch polybar
-echo "---" | tee -a /tmp/polybar.log
-polybar main_bar | tee -a /tmp/polybar.log & disown
