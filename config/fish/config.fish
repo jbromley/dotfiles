4188c056b45e4331c988bc2837680d30ff7dd06d
J. Bromley <jbromley@gmail.com>
2026-02-10 20:26:22 -0800

    Set EDITOR in fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set --global EDITOR hx
+    set --global hydro_multiline true
     dprint completions fish | source
     fzf --fish | source
     eval "$(zoxide init fish)"
     mise activate fish | source
-    # source $HOME/.atuin/bin/env.fish
-    # atuin init fish | source
 end
