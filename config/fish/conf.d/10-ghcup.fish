f609915072062a991510c2cc55b6ae20fa8b0903
J. Bromley <jay@appliedinvention.com>
2026-04-24 10:48:36 -0700

    In fish only setup up Haskell if it is installed
    Don't add ghcup and cabal to the bath if the corresponding directories
    do not exist.

--- config/fish/conf.d/10-ghcup.fish
+++ config/fish/conf.d/10-ghcup.fish
@@ -1,3 +1,8 @@
-# Set up for ghcup
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH # ghcup-env
+# Set up for Haskell
+if [-d $HOME/.ghcup]
+    set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+    fish_add_path $HOME/.ghcup/bin
+end
+if [ -d $HOME/.cabal ]
+    fish_add_path $HOME/.cabal/bin
+end
